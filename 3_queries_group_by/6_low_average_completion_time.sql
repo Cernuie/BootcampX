01176a5c3702b9cba98068fac3fc1e588187307a
Alexander Nguyen <cernvii@gmail.com>
2021-02-02 02:04:01 +0000

    3 - query 6 low avg completion done

---
+++ 3_queries_group_by/6_low_average_completion_time.sql
@@ -0,0 +1,7 @@
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
+FROM assignment_submissions
+JOIN assignments ON assignments.id = assignment_id
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY average_assignment_duration ASC;
