bba2807c726e7811a415dd68a585244df4548d72
Alexander Nguyen <cernvii@gmail.com>
2021-02-04 02:28:08 +0000

    parameterized entries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,5 @@
+args = process.argv.slice(2);
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,15 +9,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryObj =
+`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+JOIN cohorts on cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+
+
+const cohortName = args[0];
+const limit = args[1] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryObj, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+})
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(
-`
-SELECT teachers.name as teacher, cohorts.name as cohorts
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [cohortName];
+
+
+const queryStr = `
+SELECT teachers.name as teacher, cohorts.name as cohorts, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name = $1
 GROUP BY teachers.name,cohorts.name
 ORDER BY teacher;
-`)
+`
+pool.query(queryStr, values)
 .then(res => {
   res.rows.forEach(cohort => {
     console.log(`${cohort.cohorts}: ${cohort.teacher}`)
